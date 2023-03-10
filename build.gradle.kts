b91a3fe85a6af4b424b0b3d12c241dfe78e4a42a
Florian Czachor <if22b167@technikum-wien.at>
2023-03-10 13:02:12 +0100

    Magic

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,12 @@ val chartsVersion: String by project
 plugins {
     kotlin("jvm") version "1.8.0"
     application
-    id("org.openjfx.javafxplugin") version "0.0.9"
+    id("org.openjfx.javafxplugin") version "0.0.13"
 }
 
 javafx {
     version = "15.0.1"
-    modules = listOf("javafx.controls")
+    modules("javafx.controls", "javafx.swing")
 }
 
 group = "org.example"
@@ -29,6 +29,9 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation ("io.data2viz.d2v:core-jvm:$d2vVersion")
     implementation ("io.data2viz.charts:core:$chartsVersion")
+    implementation ("org.openjfx:javafx-swing:17.0.6")
+    implementation("org.glavo:simple-png:0.2.0")
+    implementation("org.glavo:simple-png-javafx:0.2.0")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,43 @@
 import io.data2viz.charts.chart.chart
 import io.data2viz.charts.chart.constant
+import io.data2viz.charts.chart.discrete
 import io.data2viz.charts.chart.mark.MarkCurves
-import io.data2viz.charts.chart.mark.line
-import io.data2viz.charts.chart.mark.plot
+import io.data2viz.charts.chart.mark.area
 import io.data2viz.charts.chart.quantitative
-import io.data2viz.charts.core.LayoutPosition
-import io.data2viz.charts.core.MarkLabel
+import io.data2viz.charts.core.*
 import io.data2viz.color.Colors
 import io.data2viz.geom.Size
-import io.data2viz.math.ticks
-import io.data2viz.shape.Symbols
+import io.data2viz.geom.point
+import io.data2viz.math.pct
+import io.data2viz.viz.FontWeight
+import io.data2viz.viz.TextHAlign
+import io.data2viz.viz.TextVAlign
 import io.data2viz.viz.newVizContainer
 import javafx.application.Application
 import javafx.scene.Scene
+import javafx.scene.image.WritableImage
 import javafx.scene.layout.Pane
 import javafx.stage.Stage
+import org.glavo.png.javafx.PNGJavaFXUtils
+import java.io.File
+import java.io.IOException
+import java.nio.file.Paths
+// from numpy import asarray
 
+// JavaFX Window Size
 private val width = 800.0
-private val height = 500.0
+private val height = 300.0
 
-val values = listOf(1.0, 2.0, 3.0, 4.0, 5.0)
-//val values2 = listOf(2.0, 3.0, 4.0, 5.0, 3.0)
+// Graph Marks Values
+// TODO: Petra Suggested "Triple" for x, y and data parameters
+// https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/-triple/
+val values = listOf(Triple(2.0, 1.0, "5A"), Triple(2.0, 2.0, "4C") , Triple(3.0, 3.0, "4B"), Triple(4.0, 4.0, "4B"))
 
 fun main() {
     Application.launch(MyFirstChart::class.java)
 }
 
-class MyFirstChart: Application() {
+class MyFirstChart : Application() {
 
     override fun start(stage: Stage) {
 
@@ -41,27 +52,102 @@ class MyFirstChart: Application() {
 
             // Create a first simple Chart
             chart(values) {
-                val values = quantitative({domain})
-                //val values2 = quantitative({domain.values2})
+                val values1 = quantitative({domain.first})
+                val values2 = discrete({domain.second})
+                val values3 = discrete({domain.third})
 
+                val startYear = 2019 - 1
+
+                // Config to disable x and y Axis
                 config {
                     yAxis { enableAxis = false }
                     xAxis { enableAxis = false }
-                    mark { label = MarkLabel.XY }
-
+                    events {
+                        highlightMode = HighlightMode.None
+                    }
                 }
 
-                line(values, values) {
+                // Create Area Graph
+                area(values2, values1) {
 
                     curve = MarkCurves.Curved
-                    size = constant(30.0)
-                    marker = constant(Symbols.Circle)
                     showMarkers = true
 
-                    strokeColor = constant(Colors.Web.black)
-                    strokeColorHighlight = constant(Colors.Web.black)
+                    // Graph Size
+                    //TODO: "val values2 = discrete({domain.second})" ignores preset Graph Size
+                    y {
+                        start = 0.0
+                        end = 10.0
+                    }
+                    x {
+                        start = .0
+                        end = 10.0
+                    }
+
+                    // Color of Graph
+                    strokeColor = constant(Colors.rgb(48,89,130))
+                    strokeColorHighlight = constant(Colors.rgb(48,89,130))
                     strokeWidth = constant(2.0)
+
+                    // Color below Graph
+                    // TODO: Improve Gradient to support different Data
+                    fill =  discrete( {
+                        Colors.Gradient.linear(point(0, 0), point(0, 800))
+                            .withColor(Colors.rgb(0,56,95, 80.pct), 0.pct)
+                            .andColor(Colors.rgb(48,89,130,0.pct), 30.pct)
+                    } )
+
+                    // Labels above and below Marks
+                    markDecorator = { datum, position, drawingZone ->
+
+                        // Call the default decorator
+                        defaultMarkDecorator(datum, position, drawingZone)
+
+                        size = constant(80.0)
+
+                        // Box for Above the Mark
+                        drawingZone.rect {
+                            x = position.x - 20.0
+                            y = position.y - 40.0
+                            width = 40.0
+                            height = 30.0
+                            fill = Colors.Web.white.withAlpha(80.pct)
+
+                            strokeColor = Colors.Web.black.withAlpha(20.pct)
+                            strokeColorHighlight = constant(Colors.rgb(48,89,130))
+                            strokeWidth = 2.0
+                        }
+                        // Text for Above the Mark
+                        drawingZone.text {
+                            x = position.x
+                            y = position.y - 25.0
+                            vAlign = TextVAlign.MIDDLE
+                            hAlign = TextHAlign.MIDDLE
+                            fontSize = 16.0
+                            // TODO: Data for textContent
+                            textContent = datum.domain.third
+                            // 5A   4C 4B 4A   3C 3B 3A   2C 2B 2A   1C 1B 1A the higher it goes
+                            // TODO: Add George Font
+                            fontWeight = FontWeight.BOLD
+                            textColor = Colors.rgb(0,56,95)
+                        }
+                        // Teyt for Below the Mark
+                        drawingZone.text {
+                            x = position.x
+                            y = position.y + 35.0
+                            vAlign = TextVAlign.MIDDLE
+                            hAlign = TextHAlign.MIDDLE
+                            fontSize = 14.0
+                            // TODO: Data for textContent
+                            textContent = "2019"
+                            // textContent = startYear + values2
+                            // TODO: Add George Font
+                            fontWeight = FontWeight.BOLD
+                            textColor = Colors.rgb(0,56,95, 60.pct)
+                        }
+                    }
                 }
+
             }
         }
 
@@ -71,5 +157,25 @@ class MyFirstChart: Application() {
             scene = (Scene(root, width, height))
             show()
         }
+
+        // Save Chart as PNG
+        val file: File? = File("chart.png")
+        if (file != null) {
+            try {
+                val writableImage = WritableImage(800, 300)
+                root.snapshot(null, writableImage)
+                /*
+                numpydata = asarray(root)
+                writeImageToArray
+                base64
+                 */
+                PNGJavaFXUtils.writeImage(writableImage, Paths.get("chart.png"))
+            } catch (ex: IOException) {
+                println("Error!")
+            }
+        }
+
+        // TODO: Uncomment stage.close() when done
+        // stage.close()
     }
 }
